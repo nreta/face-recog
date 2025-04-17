e954e640bdccabae6fdfebae6b5440cb0493d8bf
nreta <108077748+nreta@users.noreply.github.com>
2025-04-17 14:59:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -507,4 +507,4 @@ def logout():
 if __name__ == "__main__":
     threading.Thread(target=check_and_create_sheet_daily, daemon=True).start()
    
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5555)
