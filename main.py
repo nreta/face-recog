47bd519b8a1288eccfb092eb73257c230700d487
nreta <108077748+nreta@users.noreply.github.com>
2025-04-24 14:16:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -216,7 +216,7 @@ def load_known_faces_threaded():
         except Exception as e:
             print(f"❌ Error reloading faces: {str(e)}")
         
-        time.sleep(10)  # Wait 10 seconds before next reload
+        time.sleep(60)  # Wait 10 seconds before next reload
 
 # Modify your existing load_known_faces() to be thread-safe
 def load_known_faces():
