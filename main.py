4de3f321c4e6bd2c3d9d05ae49e1bbaee40b305a
nreta <108077748+nreta@users.noreply.github.com>
2025-05-10 18:13:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -318,7 +318,7 @@ def process_attendance(shift_type):
     except Exception as e:
         return jsonify({"status": "Error", "message": f"Exception: {str(e)}"})
 
-@app.route('/upload', methods=['GET'])
+@app.route('/upload_form', methods=['GET'])
 def upload_form():
     if not session.get("manager_logged_in"):  # Check if manager is logged in
         return redirect(url_for("login"))  # Redirect to login if not authorized
