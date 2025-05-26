21f6aea9f430dcd1a1207f18f6c40947b189c0df
nreta <108077748+nreta@users.noreply.github.com>
2025-05-26 18:23:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -318,7 +318,7 @@ def process_attendance(shift_type):
                 continue
 
             best_match_index = np.argmin(face_distances)
-            if face_distances[best_match_index] < 0.35:
+            if face_distances[best_match_index] < 0.45:
                 name = local_names[best_match_index]
                 current_time = datetime.now(ZoneInfo("Europe/Moscow")).strftime("%H:%M")
 
